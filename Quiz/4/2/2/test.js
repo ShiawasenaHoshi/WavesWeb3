79debc692a8a89dfb0e9c617a909ae8391aa0d79
Vasily <shiawasena.hoshi@gmail.com>
2019-06-09 11:25:09 +0300

    Quiz 4.2.2

---
+++ Quiz/4/2/2/test.js
@@ -0,0 +1,33 @@
+/**
+ * Created by vasily on 09.06.19.
+ */
+let datajson = {
+    "Title": "Waves t-Shirt with love, vote 1",
+    "coupon_price": 10000000,
+    "old_price": 1000000000,
+    "new_price": 100000000,
+    "address": "Universe",
+    "description": "In theory there is no difference between theory and practice. In practice there is. (Yogi Berra)",
+    "image": "http://bit.ly/2WSUODH"
+};
+
+let accountSupplierSeed = "fluid easily catalog few vacuum soldier bar milk rather alone prize trip";
+let dappAddress = "3MvWngGcbddUagYREyqeAKSFNw3H92w7LS4";
+
+it('add item', async function () {
+    let ts = invokeScript({
+            dApp: dappAddress,
+            call: {
+                function: "addItem",
+                args: [
+                    {type: "string", value: datajson.Title},
+                    {type: "integer", value: datajson.coupon_price},
+                    {type: "string", value: JSON.stringify(datajson)}
+                ]
+            },
+            payment: []
+        }, accountSupplierSeed
+    );
+    let tx = await broadcast(ts);
+    await waitForTx(tx.id)
+});
